8bfb9d631d65472fd367696c2f74fc31fb543fb4
Unknown <higorcp18@gmail.com>
2018-09-05 10:34:27 -0300

    Created new challenge

---
+++ AulaGridView/app/src/main/java/ucb/br/aulagridview/GridViewActivity.java
@@ -0,0 +1,13 @@
+package ucb.br.aulagridview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GridViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_view);
+    }
+}
